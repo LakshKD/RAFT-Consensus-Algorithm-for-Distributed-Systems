42e0221b496af533d32b0babcf7e0d22e7592cdb
LakshKD <lakshya@cse.iitb.ac.in>
2016-01-21 20:21:16 +0530

    Initial commit

---
+++ assignment1/README.md
@@ -0,0 +1,84 @@
+Name    :- Lakshya Kumar
+Roll No :- 153050051
+
+Description of the FileServer Written in Go language
+
+-> The below mentioned operations can be performed on the file server :-
+
+ 	1. read   (reading the contents of the file)
+ 	2. write  (Writing into the file )
+ 	3. cas    (Compare and swap)
+ 	4. delete (Delete the file from the file server)
+
+
+-> All the files are stored in the file server's secondary storage and there is a Map data structure that is used to index the files. The structure of the map is as follows:-
+
+   type Filestats struct {
+     version int64
+     numofbytes int
+     exptime int64
+     ctime int64	
+   }
+  var files map[string]Filestats
+
+-> So each file name has 4 fields that are :-
+
+	1.version.
+	2.numofbytes(size of the file).
+	3.exptime(time at which the file get expired).
+	4.ctime (the creation time of the file).
+
+-> Inside serverMain() function there are two functions in the fileserver.go file :-
+  1. RequestHandler()     :-   Handle request of each client
+  2. FileExpiryhandler()  :-   Time to time deletes the files form the server's secondary storage whose 
+                                   creation time + expiry time < current time
+
+Note :- Inside the RequestHandler all the four commands i.e read,write,cas and delete are implemented. 
+
+-> Concurrency
+     *In order to handle the concurrency i have used the sync package that will provide the lock() and unlock() function. Whenever there is some updations peformed on the file then the client will acquire the locks.
+
+     *Whenever the updations are performed on the map then also the clients will acquire the locks().
+
+
+
+Assumptions:-
+
+-> Map is always present in the primary memory and the server is always up. The changes which we are making in the map  are not persistent so when the server goes down then the changes done to the map will get lost. 
+
+-> Initially, there are 4 files present on the server that are test1.txt,test2.txt,test3.txt and test4.txt.
+   There entries are also present in the map.
+
+Assumptions on file expiry time:-
+
+In case of write command 
+   -> When the write command is like "write filename numbytes \r\n content bytes\r\n"
+       
+     case-1(File exists on the server)
+        *> Then directly write to the file and the expiry time and creation time will remain same.
+
+     case-2(File does not exists on the server)
+        *> Then file will be created with 0 expiry time and creation time will be the current time of the server.  
+
+   -> When the write command is like "write filename numbytes exptime\r\n content bytes\r\n"   
+
+     case-1(File exists on the server)   
+        *> Then directly write to the file and the expiry time and creation time will remain same.
+
+     case-1(File does not exists on the server)   
+        *> Then file will be created with expiry time as "exptime" and creation time will be the current time of the server. This file will gets deleted when the creation time (ctime) + exptime of this file is less than the current time of the server. 
+
+Testing the Server:-
+
+ 1. I have tested the server with the file basic_test.go.
+ 2. I have checked the file server with "go test" then OK is coming.
+ 3. I have checked the file server with "go test -race" then there is one Race condition is coming. 
+
+ Reference:-
+
+ 1. https://tour.golang.org/moretypes/16
+ 2. https://golang.org/pkg/strings/#example_LastIndex
+ 3. https://gobyexample.com/epoch
+ 4. https://blog.golang.org/go-maps-in-action
+ 5. https://gobyexample.com/maps
+ 6. http://loige.co/simple-echo-server-written-in-go-dockerized/
--- assignment1/fileserver.go
+++ assignment1/fileserver.go
@@ -4,49 +4,69 @@ import (
     "fmt"
     "net"
     "os"
-    //"io"
     "io/ioutil"
-   //"strconv"
-     "strings" 
-    //"bytes"
-     "strconv"	
+    "strings" 
+    "strconv"
+    "sync"
+    "time"
+    //"sync/atomic"	
 )
 
 const (  
-    HOST = ""
-    PORT = "8089"
-    TYPE = "tcp"
-    lb = "<"
-    rb = ">"  
+    HOST = "127.0.0.1"
+    PORT = "8080"
+    TYPE = "tcp"  
 )
-
 type Filestats struct {
      version int64
      numofbytes int
-     exptime int	
+     exptime int64
+     ctime int64	
+}
+var files map[string]Filestats
+
+func FileExpiryhandler(m map[string]Filestats) (){
+    var mutex = &sync.Mutex{}
+   for k, _ := range m {
+     if(m[k].exptime!=0){
+                                                                          //Delete file when
+          if((m[k].ctime+m[k].exptime)<time.Now().Unix()){  
+                mutex.Lock()                                                          //creation time plus expiry time is less than current time
+                os.Remove(k)
+                delete(m,k) 
+                mutex.Unlock() 
+          }
+     }
+
+   }   
 
 }
 
-var files map[string]Filestats
 
+ 
 func serverMain(){
-  files = make(map[string]Filestats)
-  files["test1.txt"] = Filestats {
-     0,6,0,
+
+     
+   
+   files = make(map[string]Filestats)
+   files["test1.txt"] = Filestats {
+     0,6,0,time.Now().Unix(),
   }
   files["test2.txt"] = Filestats {
-     0,8,0,
+     0,8,0,time.Now().Unix(),
   }
   files["test3.txt"] = Filestats {
-     0,10,0,
+     0,10,0,time.Now().Unix(),
   }
   files["test4.txt"] = Filestats {
-     0,4,0,
+     0,4,0,time.Now().Unix(),
  }
+   
+   
    p, err := net.Listen(TYPE, ":"+PORT)   // Listen for incoming connections.
     if err != nil {
         fmt.Println("Error on listening:", err.Error())
-        os.Exit(1)
+        //os.Exit(1)
     }
     
     defer p.Close()  // Close the listener when the application closes.
@@ -56,63 +76,248 @@ func serverMain(){
         conn, err := p.Accept()  // Listen for an incoming connection.
         if err != nil {
             fmt.Println("Error accepting connections ", err.Error())
-            os.Exit(1)
+            //os.Exit(1)
         }
 
-        go RequestHandler(conn)// Handle request
-	
+         go RequestHandler(conn)// Handle request
+	       go FileExpiryhandler(files)
     }
 }
 
 
-func RequestHandler(conn net.Conn) {  
-  readbuf := make([]byte, 1024)   // Make a buffer to hold incoming data.
-  size, err := conn.Read(readbuf)  // Read the incoming connection into the buffer.
+func RequestHandler(conn net.Conn) { 
+  var mutex = &sync.Mutex{}
+  //mux sync.Mutex{};
+  for {    
+  readbuf := make([]byte, 2048)
+  size, err := conn.Read(readbuf)  
   if err != nil {
     fmt.Println("Error in reading from the read buffer:", err.Error())
+    conn.Close()
+    return 
   }
-   s := string(readbuf[:size])    //Converting into the string
-   p := strings.Fields(s)        //Splitting the string s into the fields in order to extract the Command
+   s := string(readbuf[:size])    
+      //fmt.Println(s)                           
+   //j := strings.Split(s,"\r\n") 
+     j := strings.SplitN(s,"\r\n",2)
+     //fmt.Println(j)
+     j[1] = strings.Trim(j[1], "\r\n")
+      //fmt.Println(j[1])
+   p := strings.Split(j[0]," ")	
+      //fmt.Println(p)
+   if p[0] == "read"{
 
-  if p[0] == "read"{
-   data, err := ioutil.ReadFile(p[1])   //Reading the file,the filename is in the p[1]
-     if err != nil {
+    cmdlen := len(p)
+    //fmt.Println(cmdlen)
+    if cmdlen==2{
+          //fmt.Println("Inside read")
          
-            conn.Write([]byte("ERR_FILE_NOT_FOUND\n"))    //Sending the error message on the channel to the client
-           os.Exit(1)
-     } 
-    conn.Write([]byte("CONTENTS "))
-    conn.Write([]byte(lb + strconv.FormatInt(files[p[1]].version,10) + rb + " ")) 
-    conn.Write([]byte(lb + strconv.Itoa(files[p[1]].numofbytes) + rb + " "))
-    conn.Write([]byte(lb + strconv.Itoa(files[p[1]].exptime) + rb + "\\r\\n")) 
-    conn.Write([]byte("\n")) 
-    conn.Write([]byte(data)) // Write the file content in the connection channel.
-    conn.Write([]byte("\\r\\n"))
-     }else if p[0] == "write"{
+           mutex.Lock()
+           data, err := ioutil.ReadFile(strings.TrimSpace(p[1]))
+          if err != nil {
+         
+            conn.Write([]byte("ERR_FILE_NOT_FOUND\n")) 
+            conn.Close()   
+          } 
           
-         if _, err := os.Stat(p[1]); err == nil {
-	 z := strings.Split(s,"\\r\\n")
-         ioutil.WriteFile(p[1],[]byte(z[1]),0644)
-	 conn.Write([]byte("OK <version>\r\n"))     
-         } else {
-          conn.Write([]byte("ERR_FILE_NOT_FOUND\n"))
-	    os.Exit(1)
-          } 	   
+          // fmt.Println("dsfsdfds")
+           
+           ver     :=   strconv.FormatInt(files[p[1]].version,10)
+           nmbytes :=   strconv.Itoa(files[p[1]].numofbytes)
+           exp     :=   strconv.FormatInt(files[p[1]].exptime,10) 
+            
+           conn.Write([]byte("CONTENTS"+" "+ver+" "+nmbytes+" "+exp+" "+"\r\n"))
+           conn.Write([]byte(data))
+          //fmt.Println("heyo")
+          //fmt.Println(files)
+          conn.Write([]byte("\r\n"))
+          mutex.Unlock()
+        }else{
+
+          conn.Write([]byte("ERR_CMD_ERR\n"))
+        }
+
+     }else if p[0] == "write"{
+           cmdlen := len(p)
+           if cmdlen == 3 || cmdlen == 4 {
+
+                if cmdlen == 3{
+
+                  if _, err := os.Stat(p[1]); err == nil {   //File Exists on the server
+	                 //fmt.Println("inside write")
+                  mutex.Lock()
+                  ioutil.WriteFile(p[1],[]byte(j[1]),0777)    
+		              b, _ := strconv.Atoi(p[2])
+		              expt := files[p[1]].exptime
+                  c1time:= files[p[1]].ctime
+                  files[p[1]] = Filestats {
+                    //0,b,expt,time.Now().Unix(),
+                    0,b,expt,c1time,
+                  }       	   
+             
+                  s := strconv.FormatInt(files[p[1]].version,10)
+                  conn.Write([]byte("OK"+" "+s+"\r\n"))    
+                  mutex.Unlock()
+
+                  } else { 
+                            mutex.Lock()  
+                            //fmt.Println("hello")         //File does not Exists on the server
+                            ioutil.WriteFile(p[1],[]byte(j[1]),0777)   
+						                                       
+                            b, _ := strconv.Atoi(p[2])
+		                        expt := int64(0)
+                            //expt , _ := strconv.ParseInt(p[3],10,64)                 
+		           
+                    
+                            files[p[1]] = Filestats {
+                                0,b,expt,time.Now().Unix(),
+                            }  
+                    
+		                       s := strconv.FormatInt(files[p[1]].version,10)     
+		                        conn.Write([]byte("OK" +" " +s+"\r\n"))
+	                         mutex.Unlock()                                                                     
+                          } 
+                 }else if cmdlen==4{
+                       if _, err := os.Stat(p[1]); err == nil {   //File Exists on the server
+                   //fmt.Println("inside write")
+                  mutex.Lock()
+                  ioutil.WriteFile(p[1],[]byte(j[1]),0777)    
+                  b, _ := strconv.Atoi(p[2])
+                  expt := files[p[1]].exptime
+                  c1time := files[p[1]].ctime
+                  files[p[1]] = Filestats {
+                    //0,b,expt,time.Now().Unix(),
+                    0,b,expt,c1time,
+                  }            
+             
+                  s := strconv.FormatInt(files[p[1]].version,10)
+                  conn.Write([]byte("OK"+" "+s+"\r\n"))    
+                  mutex.Unlock()
+
+                  } else { 
+                            mutex.Lock()  
+                            //fmt.Println("hello")         //File does not Exists on the server
+                            ioutil.WriteFile(p[1],[]byte(j[1]),0777)   
+                                                   
+                            b, _ := strconv.Atoi(p[2])
+                            
+                            expt , _ := strconv.ParseInt(p[3],10,64)                 
+                             //expt = expt + time.Now().Unix()
+                    
+                            files[p[1]] = Filestats {
+                                0,b,expt,time.Now().Unix(),
+                            }  
+                    
+                           s := strconv.FormatInt(files[p[1]].version,10)     
+                            conn.Write([]byte("OK" +" " +s+"\r\n"))
+                           mutex.Unlock()  
+
+                         }
+
+                 }
+           }else{
+                  conn.Write([]byte("ERR_CMD_ERR\n"))
+            }
+
+             
      }else if p[0] == "cas"{
 
+        cmdlen := len(p)
+           if cmdlen == 4 || cmdlen == 5 {
+          if cmdlen == 4{
+            if _, err := os.Stat(p[1]); err == nil {
 
+              s := strconv.FormatInt(files[p[1]].version,10)
+              if s == strings.TrimSpace(p[2]){
+                   mutex.Lock()
+                   ioutil.WriteFile(p[1],[]byte(j[1]),0777)
+                    v   := files[p[1]].version
+                    b,_ := strconv.Atoi(strings.TrimSpace(p[3]))            //numofbytes
+                    e,_ := strconv.ParseInt(strings.TrimSpace(p[4]),10,64)           //exptime
+                    delete(files,p[1])
+                    v=v+1                                 //Updating the version
+                    files[p[1]] = Filestats {            //Creating a new entry for the file in the map
+                     v,b,e,time.Now().Unix(),
+                  } 
+                  s1 := strconv.FormatInt(files[p[1]].version,10)     
+                  conn.Write([]byte("OK" +" "+s1+"\r\n"))
+                    mutex.Unlock()
+               }else {
+                   conn.Write([]byte("ERR_VERSION "))
+                   conn.Write([]byte(s+"\r\n"))
+                   conn.Close()
+                   return
+               }
+          }else{
+           conn.Write([]byte("ERR_FILE_NOT_FOUND\n"))    
+             conn.Close()
+             return     
+          }
+        }else if cmdlen == 5{
+            if _, err := os.Stat(p[1]); err == nil {
+
+              s := strconv.FormatInt(files[p[1]].version,10)
+              if s == strings.TrimSpace(p[2]){
+                   mutex.Lock()
+                   ioutil.WriteFile(p[1],[]byte(j[1]),0777)
+                    v   := files[p[1]].version
+                    b,_ := strconv.Atoi(strings.TrimSpace(p[3]))            //numofbytes
+                    e,_ := strconv.ParseInt(strings.TrimSpace(p[4]),10,64)           //exptime
+                    delete(files,p[1])
+                    v=v+1                                 //Updating the version
+                    files[p[1]] = Filestats {            //Creating a new entry for the file in the map
+                     v,b,e,time.Now().Unix(),
+                  } 
+                  s1 := strconv.FormatInt(files[p[1]].version,10)     
+                  conn.Write([]byte("OK" +" "+s1+"\r\n"))
+                    mutex.Unlock()
+               }else {
+                   conn.Write([]byte("ERR_VERSION "))
+                   conn.Write([]byte(s+"\r\n"))
+                   conn.Close()
+                   return
+               }
+          }else{
+           conn.Write([]byte("ERR_FILE_NOT_FOUND\n"))    
+             conn.Close()
+             return     
+          }
+           
+
+        }
+      }else{
+           conn.Write([]byte("ERR_CMD_ERR\n"))    
+          }
+     
      }else if p[0] == "delete"{
 
-  
+        cmdlen := len(p)
+        if cmdlen == 2{ 
+       if _, err := os.Stat(strings.TrimSpace(p[1])); err == nil{
+             mutex.Lock()
+             os.Remove(p[1])
+              conn.Write([]byte("OK" + "\r\n")) 
+             mutex.Unlock()
+         }else{
+            conn.Write([]byte("ERR_FILE_NOT_FOUND\n"))
+            conn.Close()
+            return
+         } 
+       }else{
+        conn.Write([]byte("ERR_CMD_ERR\n"))
+       }
+
+     }else {
+
+        conn.Write([]byte("ERR_CMD_ERR\n"))
+
      }
-  // Close the connection when you're done with it.
-  conn.Close()
+   }
 
 
 }
-
 func main() {  
-   serverMain()    //Calling FileServer 
+  serverMain()    
 }
 
 
