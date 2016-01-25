c34534b674646e94dfc34df0a1f314f21f575ad4
LakshKD <lakshya@cse.iitb.ac.in>
2016-01-25 23:30:38 +0530

    Update basic_test.go

--- assignment1/basic_test.go
+++ assignment1/basic_test.go
@@ -7,25 +7,36 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+       "time"
+	"sync"
 )
-
 
 // Simple serial check of getting and setting
 func TestTCPSimple(t *testing.T) {
-	go serverMain()
-        //time.Sleep(1 * time.Second) // one second is enough time for the server to start
-	name := "hi.txt"
-	contents := "bye afnla fdalnadfa"
-	exptime := 300000
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		t.Error(err.Error()) // report error through testing framework
-	}
+	var mutex = &sync.Mutex{}
+        go serverMain()
+          var wg sync.WaitGroup
+        time.Sleep(1 * time.Second) // one second is enough time for the server to start
+         
+         wg.Add(5)
+ 
+         for i:=1 ; i<=5 ; i++ {
+          
+         go func (j int) {
+        //fmt.Println("dfjnsdjfn")
+        name     := "hi.txt"
+	contents := "Hey"
+	exptime  := 300000
+	conn, err:= net.Dial("tcp", "localhost:8080")
 
-	scanner := bufio.NewScanner(conn)
+		if err != nil {
+			t.Error(err.Error()) // report error through testing framework
+		}
+        //fmt.Println(conn)
+	scanner  := bufio.NewScanner(conn)
 
-	// Write a file
-	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents)
+	// Writing to a file
+	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents+strconv.Itoa(j))
 	scanner.Scan() // read first line
 	resp := scanner.Text() // extract the text from the buffer
 	arr := strings.Split(resp, " ") // split into OK and <version>
@@ -34,8 +45,11 @@ func TestTCPSimple(t *testing.T) {
 	if err != nil {
 		t.Error("Non-numeric version found")
 	}
+	
 
-	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
+
+        // try a read now
+	fmt.Fprintf(conn, "read %v\r\n", name) 
 	scanner.Scan()
 
 	arr = strings.Split(scanner.Text(), " ")
@@ -46,30 +60,38 @@ func TestTCPSimple(t *testing.T) {
 	//fmt.Println(scanner.Text())
 	expect(t, contents, scanner.Text())
 
-    //Compare And Swap Testing
-   
-    contents = "hi this is newvjhvjhvhvjh contents"
-     version1 := 0
-    fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name,version1,len(contents), exptime, contents)
-     scanner.Scan()
-     resp = scanner.Text()
-     //fmt.Println(resp)
-     arr = strings.Split(resp, " ")
-     expect(t, arr[0], "OK")
+        //Compare And Swap Testing
+       mutex.Lock()
+       contents = "New Contents"
+       version1 := 0
+       fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name,version1,len(contents), exptime, contents+strconv.Itoa(j))
+       scanner.Scan()
+       resp = scanner.Text()
+       fmt.Println(resp)
+       arr = strings.Split(resp, " ")
+       expect(t, arr[0], "OK")
 	 version, err = strconv.ParseInt(arr[1], 10, 64) // parse version as number
 	 if err != nil {
 		t.Error("Non-numeric version found")
 	 }
-    
+         time.Sleep(2 * time.Second)
+    mutex.Unlock()
      // Delete the file
-   
-	 fmt.Fprintf(conn, "delete %v\r\n", name) 
+        
+	/* fmt.Fprintf(conn, "delete %v\r\n", name) 
 	 scanner.Scan()
 	 resp = scanner.Text()
 	 //fmt.Println(resp)
      arr = strings.Split(resp, " ")
-     expect(t, arr[0], "OK")
+     expect(t, arr[0], "OK")*/
+
+      wg.Done()
+        }(i)
      
+     }
+    wg.Wait()
+   //time.Sleep(2 * time.Second)
+  
 }
 
 // Useful testing function
